c8d00b39b3f76294c287d343cc4e2b513d1355b5
n4beel <nabeel3353045805@hotmail.com>
2022-06-11 14:59:04 +0500

    SCREEN DATA updated

--- components/Gallery.js
+++ components/Gallery.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, ScrollView} from 'react-native';
 import shuffle from 'lodash.shuffle';
 import GalleryItem from './GalleryItem';
-import {DEFAULT_ITEMS} from '../utils/DefaultItems';
+import {useFocusEffect} from '@react-navigation/native';
 
 const Gallery = ({rowNumber}) => {
-  const items = shuffle(DEFAULT_ITEMS);
+  const [items, setItems] = useState([]);
+
+  useFocusEffect(() => {
+    (async () => {
+      let data = await fetch(
+        'https://raw.githubusercontent.com/n4beel/webview-example/main/SCREENS_DATA.js',
+      );
+      data = await data.json();
+      console.log('data => ', data);
+    })();
+  });
 
   return (
     <ScrollView horizontal style={styles.row}>
-      {items.map((item, i) => (
-        <GalleryItem
-          key={i}
-          title={item.name}
-          image={item.image}
-          screens={item.screens}
-          hasTVPreferredFocus={rowNumber === 0 && i === 0}
-          blockFocusRight={i === items.length - 1}
-        />
-      ))}
+      {items &&
+        items.map((item, i) => (
+          <GalleryItem
+            key={i}
+            title={item.name}
+            image={item.image}
+            screens={item.screens}
+            hasTVPreferredFocus={rowNumber === 0 && i === 0}
+            blockFocusRight={i === items.length - 1}
+          />
+        ))}
     </ScrollView>
   );
 };
